fdc60adbf3f9c3cd718ba18735eacf9e8a24232a
PedroSiqueira <pedro.siqueira@ifms.edu.br>
2023-08-06 11:39:05 -0300

    modified:   keyboard_abnt2_like_linux.py

--- keyboard_abnt2_like_linux.py
+++ keyboard_abnt2_like_linux.py
@@ -2,68 +2,75 @@
 mapeia algumas teclas do teclado abnt2 (que contém a tecla altgr) no windows como no linux, por exemplo, altgr+y é mapeado para ←
 """
 
-from enum import Enum
-from pynput.keyboard import Key, KeyCode, Controller as KeyController, Listener as KeyListener
-import time
+from pynput import keyboard
+
+controller = keyboard.Controller()
 
 
-class Keyboard(Enum):
-    Escape = 'Escape'
-    AltRight = 'AltRight'
+def left_arrow():
+    controller.type('←')
 
 
-def get_key(key):
-    if isinstance(key, KeyCode):
-        if key.char is not None:
-            return key.char
-        elif key.vk is not None:
-            return key.vk
-        else:
-            return None
-    elif isinstance(key, Key):
-        if key == Key.alt_gr:
-            print("AltGr (Right Alt) key pressed.")
-        return {
-            27: Keyboard.Escape,
-            165: Keyboard.AltRight
-        }.get(key.value)
-    else:
-        return 'i dont know'
+def down_arrow():
+    controller.type('↓')
 
 
-def tap_button(key, times=1, delay=0.005):
-    for _ in range(times):
-        keyboard.tap(key)
-        if delay > 0.0:
-            time.sleep(delay)
+def up_arrow():
+    controller.type('↑')
 
 
-def on_press(key):
-    if keyboard.alt_gr_pressed:
-        print('altgr')
-    return
-    if get_key(key) == Keyboard.Escape:
-        return False
-    keys.keys[get_key(key)] = True
+def right_arrow():
+    controller.type('→')
 
 
-def on_release(key):
-    if keyboard.alt_gr_pressed:
-        print('altgr')
-    # print(get_key(key), keys.keys[get_key(key)])
-    # keys.keys[get_key(key)] = False
-    if get_key(key) == 'y':
-        print('y')
+def grave_accent():
+    controller.type('`')
 
 
-class Keys:
-    def __init__(self) -> None:
-        self.keys = {}
+def left_double_angle_quote():
+    controller.type('«')
 
 
-if __name__ == '__main__':
-    keys = Keys()
-    keyboard = KeyController()
-    keyboard_listener = KeyListener(on_press=on_press, on_release=on_release)
-    keyboard_listener.start()
-    keyboard_listener.join()
+def right_double_angle_quote():
+    controller.type('»')
+
+
+def left_single_angle_quote():
+    controller.type('‹')
+
+
+def right_single_angle_quote():
+    controller.type('›')
+
+
+def left_double_quote():
+    controller.type('“')
+
+
+def right_double_quote():
+    controller.type('”')
+
+
+def left_single_quote():
+    controller.type('‘')
+
+
+def right_single_quote():
+    controller.type('’')
+
+
+with keyboard.GlobalHotKeys({
+    '<alt_gr>+y': left_arrow,
+    '<alt_gr>+u': down_arrow,
+    # não sei se é um bug ou 'feature' do pynput, mas seu eu pressionar <shift>+<alt_gr>+u, ele também dispara a hotkey <alt_gr>+shift+u
+    '<shift>+<alt_gr>+u': up_arrow,
+    '<alt_gr>+i': right_arrow,
+    '<alt_gr>+´': grave_accent,
+    '<alt_gr>+z': left_double_angle_quote,
+    '<alt_gr>+x': right_double_angle_quote,
+    '<alt_gr>+v': left_double_quote,
+    '<alt_gr>+b': right_double_quote,
+    '<shift>+<alt_gr>+v': left_single_quote,
+    '<shift>+<alt_gr>+b': right_single_quote,
+}) as h:
+    h.join()
